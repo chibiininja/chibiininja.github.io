3fcbe73e58c618399591969965f0c61a01ac2ec9
Darren Inouye <dinouye@scu.edu>
2025-07-15 06:32:18 -0700

    fixed bluesky link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     return (
         <div className="Lower">
             <a href="https://chibiininja.itch.io/">itch.io</a>
-            <a href="https://https://bsky.app/profile/chibiininja.itch.io">Bluesky</a>
+            <a href="https://bsky.app/profile/chibiininja.itch.io">Bluesky</a>
             <a href="https://instagram.com/chibiininja/">Instagram</a>
             <a href="https://www.linkedin.com/in/darren-inouye/">LinkedIn</a>
             <a href="https://github.com/chibiininja">GitHub</a>
